8b7a843d16b43ab72665923a84b48345c6f36cea
dgupta <dgupta@ebi.ac.uk>
2020-04-26 20:49:48 +0100

    Monor changes to button name, logo etc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
-
 
 @NgModule({
   declarations: [],
